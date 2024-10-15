4622625d6ef8bde97ea8e10cea7deb7bfa3ab4d0
d08241002 <d08241002@ntu.edu.tw>
2024-10-15 11:34:11 +0800

    no good

--- R_data/My_types.R
+++ R_data/My_types.R
@@ -3,14 +3,14 @@ plants<-rep(x=1:5,time=2)
 height<- c(4,5,3,6,7,9,6,5,4,7)
 my.experiment<-data.frame (f_time, plants,height)
 my.experiment
-factor(c("1","2"),levels = c("1","2"))
 
-x<-c("f_time","plants")
-y=as.numeric(height) 
+dheight<- as.double(height)
+typeof(dheight)
+x<-c("f_time", "plants")
+y<-as.double(height)
+x.f<-as.factor(y)
 growth<-data.frame (x,y)
 growth
 
-x<-c(c("1","2"))
-y=as.numeric(height) 
-growth<-data.frame (x,y)
-growth
+
+x.f
